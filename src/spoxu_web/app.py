7c04cb5c91acfe130dfa3d12d45ef1d39c42f865
juniorxo <wilsongrana2@gmail.com>
2026-07-12 20:00:07 -0500

    Apply Ruff import grouping

--- src/spoxu_web/app.py
+++ src/spoxu_web/app.py
@@ -15,6 +15,7 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from spotify_scheduler_pro.models import ScheduleEntry, ScheduleKind
 from spotify_scheduler_pro.scheduler import SchedulerEngine
+
 from .config import WebSettings
 from .runtime import WebRuntime
 
